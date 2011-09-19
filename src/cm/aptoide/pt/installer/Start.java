d3c11fe80e790a839779af900a163a7488bcc950
dsilveira <dsilveira@8d55eb69-6847-4b75-98a7-935b3913b0ec>
2011-09-19 18:41:20 +0000

    added project to repo
    git-svn-id: http://aptoide.org/repo/aptoide-installer@228 8d55eb69-6847-4b75-98a7-935b3913b0ec

---
+++ src/cm/aptoide/pt/installer/Start.java
@@ -0,0 +1,20 @@
+package cm.aptoide.pt.installer;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+public class Start extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+
+    	String weburl = "http://m.aptoide.com";
+    	Intent myintent = new Intent(Intent.ACTION_VIEW);
+    	myintent.setData(Uri.parse(weburl));
+    	startActivity(myintent);
+    	
+    	finish();
+    }
+}
